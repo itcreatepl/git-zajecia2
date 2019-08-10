6de5b49bbbae8f8a98fb7ddd320e67cf318c928e
Piotr Hołownia <biuro@itcreate.pl>
2019-08-10 10:38:21 +0200

    teraz

--- src/Application.java
+++ src/Application.java
@@ -2,5 +2,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello");
         String myString = "git asdsa";
+        String othetr = "no ej ";
+
+        Integer intele = 1;
+
+        System.out.println("dsadasdasd");
+
+
     }
 }
