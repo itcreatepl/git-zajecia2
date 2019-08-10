446554074c1438ece9d3b1d88ddc3ec364f3706f
Piotr Hołownia <biuro@itcreate.pl>
2019-08-10 11:56:45 +0200

    feat add

--- src/Application.java
+++ src/Application.java
@@ -2,15 +2,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello");
         String myString = "git asdsa";
-        String othetr = "no ej ";
 
-        Integer intele = 1;
-
-        System.out.println("dsadasdasd");
-
-        System.out.println("tera");
-        System.out.println("nowy branch");
-        System.out.println("Zmina");
 
 
 
